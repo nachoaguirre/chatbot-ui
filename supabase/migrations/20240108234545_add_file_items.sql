43394897112672254289bf06d13c223d6cd63c4d
Ignacio Aguirre <ignacio@independiente.cl>
2026-05-25 23:16:16 -0400

    fix

--- supabase/migrations/20240108234545_add_file_items.sql
+++ supabase/migrations/20240108234545_add_file_items.sql
@@ -1,8 +1,10 @@
+SET search_path = public, extensions;
+
 --------------- FILE ITEMS ---------------
 
 create table file_items (
   -- ID
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 
   -- RELATIONSHIPS
   file_id UUID NOT NULL REFERENCES files(id) ON DELETE CASCADE,
